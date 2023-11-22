3ff9cc0d49d92542c5f7cc915ac2462f3cf0739c
rajalakshmyb999@gmail.com <rajalakshmyb999@gmail.com>
2023-11-22 18:34:25 +0530

    h

--- First_Project/cruds/src/main/java/com/exam/cruds/Controller/AccountController.java
+++ First_Project/cruds/src/main/java/com/exam/cruds/Controller/AccountController.java
@@ -4,10 +4,7 @@ package com.exam.cruds.Controller;
 import com.exam.cruds.Model.AccountModel;
 import com.exam.cruds.Repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +21,11 @@ public class AccountController {
 
         return repo.findAll();
 
+    }
+    @GetMapping("/book/{bookid}")
+    public List<AccountModel> getBooks(@PathVariable("accountid") int accountid)
+    {
+        return booksService.getBooksById(bookid);
     }
 
     @GetMapping("/SelectAll")
