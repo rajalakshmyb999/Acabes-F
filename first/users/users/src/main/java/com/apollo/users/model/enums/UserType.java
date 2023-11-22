89aa6a01acdbde8de1c10e2a342715acca6b8670
rajalakshmyb999@gmail.com <rajalakshmyb999@gmail.com>
2023-11-22 18:48:42 +0530

    Add

--- first/users/users/src/main/java/com/apollo/users/model/enums/UserType.java
+++
@@ -1,5 +0,0 @@
-package com.apollo.users.model.enums;
-
-public enum UserType {
-    DOCTOR,NURSE,OPERATIONAL_STAFF,PATIENT;
-}
--- first/users/users/src/main/java/com/apollo/users/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.apollo.users.repository;
-
-import com.apollo.users.entitiy.UserEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<UserEntity, String> {
-
-}
